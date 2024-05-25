e5baefea33b0b281f9c05cad28bbda2c0b8a925e
VaishnaviChouksey <vaishnavichouksey9@gmail.com>
2024-05-26 01:07:54 +0530

    commit

---
+++ controllers/userController.js
@@ -0,0 +1,78 @@
+const User=require("../models/userModel");
+const bcryptjs=require("bcryptjs");
+
+module.exports.register=async (req,res,next)=>{
+  try{
+          const {username,email,password}=req.body;
+          const usernameCheck=await User.findOne({username});
+          if(usernameCheck)
+          return res.json({msg: "Username already used",status:false});
+          const emailCheck=await User.findOne({email});
+          if(emailCheck)
+          return res.json({msg: "Email already used",status:false});
+          const hashedPassword=await bcryptjs.hash(password,10);
+          const user=await User.create({
+            email,
+            username,
+            password:hashedPassword,
+          });
+          delete user.password;
+          return res.json({status:true,user});
+        }
+        catch(ex){
+          next(ex);
+        }
+} ; 
+
+module.exports.login=async (req,res,next)=>{
+  try{
+          const {username,password}=req.body;
+          const user=await User.findOne({username});
+          if(!user)
+          return res.json({msg: "Incorrect Username or Password",status:false});
+          const isPasswordValid = await bcryptjs.compare(password, user.password);
+          if (!isPasswordValid)
+            return res.json({ msg: "Incorrect Username or Password", status: false });
+          
+          delete user.password;
+          return res.json({status:true,user});
+        }
+        catch(ex){
+          next(ex);
+        }
+} ; 
+
+module.exports.setAvatar=async (req,res,next)=>{
+try{
+const userId=req.params.id;
+const avatarImage=req.body.image;
+const userData=await User.findByIdAndUpdate(userId,{
+  isAvatarImageSet:true,
+  avatarImage,
+},
+{ new: true }
+);
+return res.json({
+  isSet:userData.isAvatarImageSet,
+  image:userData.avatarImage,
+});
+}
+catch(ex){
+next(ex);
+}
+};
+
+module.exports.getAllusers=async (req,res,next)=>{
+  try{
+      const users=await User.find({_id:{$ne: req.params.id}}).select([
+        "email",
+        "username",
+        "avatarImage",
+        "_id",
+      ]);
+      return res.json(users);
+  }
+  catch(ex){
+  next(ex);
+  }
+  }
