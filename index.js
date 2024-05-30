750fe7678ae8d33af9a0dfe6331a3198782ca976
VaishnaviChouksey <vaishnavichouksey9@gmail.com>
2024-05-31 02:04:07 +0530

    commit

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-const express=require("express");
-const cors=require("cors");
+const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoutes =require("./routes/userRoutes");
-const messageRoutes =require("./routes/messageRoutes");
-const socket=require('socket.io');
-const path=require('path');
-const app=express();
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const socket = require('socket.io');
+const path = require('path');
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
 require("dotenv").config();
- 
-app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth",userRoutes);     
-app.use("/api/messages",messageRoutes);     
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.get('*', (req, res) =>
-    res.sendFile(path.resolve('build', 'index.html'))
-  );
+// Serve the React app for any unknown routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URL)
-.then(()=>{console.log("DB Connection Successfull");
-})
-.catch(
-    (err)=>{
-        console.log(err.message);
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "*", // Allow any origin for now or set your specific client URL
+    credentials: true, 
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
     }
-)
-
-const server = app.listen(process.env.PORT,()=>{
-    console.log(`Server Started on Port ${process.env.PORT}`);
-})
-
-const io=socket(server,{
-    cors:{
-        origin:"*",
-        credentials:true,
-    },
+  });
 });
-
-global.onlineUsers=new Map();
-
-io.on("connection",(socket)=>{ 
-global.chatSocket=socket;
-socket.on("add-user",(userId)=>{
-    onlineUsers.set(userId,socket.id);   
-});
-
-socket.on("send-msg",(data)=>{   
-    const sendUserSocket=onlineUsers.get(data.to);
-    if(sendUserSocket){
-        socket.to(sendUserSocket).emit("msg-recieve",data.message);
-    }
-});
-});
-
-
