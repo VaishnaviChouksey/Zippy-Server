de5dbbe97e5a572a9557384da97721536680feec
VaishnaviChouksey <vaishnavichouksey9@gmail.com>
2024-05-30 23:43:54 +0530

    updated

--- routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const { addMessage,getMessage } = require("../controllers/messageController");
-
-const router=require("express").Router();
-
-router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getMessage);
-
-module.exports = router;    
