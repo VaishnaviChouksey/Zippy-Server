7d6e2da6bb2c72feb756c136a58afd4e206eb581
VaishnaviChouksey <vaishnavichouksey9@gmail.com>
2024-06-02 01:58:13 +0530

    new commit

--- index.js
+++ index.js
@@ -34,32 +34,30 @@ mongoose.connect(process.env.MONGO_URL)
   });
 
 const port = process.env.PORT || 5000;
-const server = app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+const server = app.listen(process.env.PORT,()=>{
+  console.log(`Server Started on Port ${process.env.PORT}`);
+})
 
-const server2 = http.createServer(app);
-
-const io = new Server(server2, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true,
+const io=socket(server,{
+  cors:{
+      origin:"*",
+      credentials:true,
   },
 });
 
-global.onlineUsers = new Map();
+global.onlineUsers=new Map();
 
-io.on("connection", (socket) => {
-  global.chatSocket = socket;
-  socket.on("add-user", (userId) => {
-    onlineUsers.set(userId, socket.id);
-  });
-
-  socket.on("send-msg", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-receive", data.message);
-    }
-  });
+io.on("connection",(socket)=>{ 
+global.chatSocket=socket;
+socket.on("add-user",(userId)=>{
+  onlineUsers.set(userId,socket.id);   
 });
+
+socket.on("send-msg",(data)=>{   
+  const sendUserSocket=onlineUsers.get(data.to);
+  if(sendUserSocket){
+      socket.to(sendUserSocket).emit("msg-recieve",data.message);
+  }
+});
+});
+
